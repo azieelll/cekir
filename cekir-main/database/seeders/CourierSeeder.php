aa5a550e3186acb53f25e11fc2e246aeb1243ab0
azieelll <145580053+azieelll@users.noreply.github.com>
2023-12-20 23:19:55 +0800

    cekir
    2209106080 Yohanes Donbosco Ndopo Woge

---
+++ cekir-main/database/seeders/CourierSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Courier;
+
+class CourierSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Courier::insert([
+            ['code' =>'jne' , 'title' => 'Jalur Nugraha Ekakurir (JNE)'],
+            ['code' =>'pos' , 'title' => 'POS Indonesia'],
+            ['code' =>'tiki' , 'title' => 'Citra Van Titipan Kilat']
+        ]);
+    }
+}
